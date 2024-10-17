a82e13dfb351d8dfeb118c0d4874f742f7419fd2
GUSTAVO MAFRA PALUSKI <gustavo.paluski@catolicasc.edu.br>
2024-10-16 21:56:17 -0300

    Commit push

--- tests/index.test.ts
+++ tests/index.test.ts
@@ -2,7 +2,7 @@ import { ext } from '../src/index';
 
 describe('numeroPorExtenso', () => {
     test('deve retornar o número por extenso corretamente', () => {
-        expect(ext(1)).toBe('zero');
+        expect(ext(0)).toBe('zero');
     });
 });
 
